5d593d0ead6331f595945185a777ca002f612383
Eloi Foley <2171948@cegepmontpetit.ca>
2026-09-18 14:44:24 -0400

    PROGRES : SignalR avancé

---
+++ PollingEtSignalR/labo.signalr.api/Hubs/UselessTasksHub.cs
@@ -0,0 +1,31 @@
+﻿using labo.signalr.api.Data;
+using labo.signalr.api.Models;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+
+namespace labo.signalr.api.Hubs
+{
+    public class UselessTasksHub : Hub
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UselessTasksHub(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public override async Task OnConnectedAsync()
+        {
+            base.OnConnectedAsync();
+            // TODO: Ajouter votre logique
+            await Clients.Caller.SendAsync("TaskList", _context.UselessTasks.ToListAsync());
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            base.OnDisconnectedAsync(exception);
+            // TODO: Ajouter votre logique
+        }
+    }
+}
