e535354a95fc37d8045ea7443bdcda5916b01dd8
Eloi Foley <2171948@cegepmontpetit.ca>
2026-09-18 14:07:03 -0400

    FCT : Polling complété

--- polling-et-signalr/app/polling/page.tsx
+++ polling-et-signalr/app/polling/page.tsx
@@ -10,27 +10,37 @@ export default function Home() {
   const [tasks, setTasks] = React.useState<UselessTask[]>([]);
 
   useEffect(() => {
-    updateTasks();
+    const interval = setInterval(() => {updateTasks}, 1000);
+    return () => clearInterval(interval);
   }, []);
 
-  function handleTaskAdd(taskName: string) {
+  async function handleTaskAdd(taskName: string) {
     // TODO On invoke la méthode pour ajouter une tâche sur le serveur (Contrôleur d'API)
+    let x = await axios.post(`http://localhost:5042/api/UselessTasks/Add?taskText=${taskName}`);
+    console.log(x);
+    updateTasks();
   }
 
-  function onTaskToggle(id: number) {
+  async function onTaskToggle(id: number) {
     // TODO On invoke la méthode pour compléter une tâche sur le serveur (Contrôleur d'API)
+    let x = await axios.get('http://localhost:5042/api/UselessTasks/Complete/' + id);
+    console.log(x);
 
     let tasksCopy : UselessTask[] = [...tasks];    
     tasksCopy.find(task => task.id === id)!.completed = true;
     setTasks(tasksCopy);
+    updateTasks();
   }
 
   async function updateTasks() {
-    let testTasks = new Array<UselessTask>(
-      { id: 1, text: "Test Task 1", completed: false },
-      { id: 2, text: "Test Task 2", completed: true });
-    setTasks(testTasks);
+    // let testTasks = new Array<UselessTask>(
+    //   { id: 1, text: "Test Task 1", completed: false },
+    //   { id: 2, text: "Test Task 2", completed: true });
+    // setTasks(testTasks);
     // TODO: Faire une première implémentation simple avec un appel au serveur pour obtenir la liste des tâches
+    let x = await axios.get('http://localhost:5042/api/UselessTasks/GetAll')
+    console.log(x);
+    setTasks(x.data);
     // TODO: UNE FOIS QUE VOUS AVEZ TESTER AVEC DEUX CLIENTS: Utiliser le polling pour mettre la liste de tasks à jour chaque seconde
   }
 
